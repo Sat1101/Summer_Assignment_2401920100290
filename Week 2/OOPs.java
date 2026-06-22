f720270847a6e2583a587648f080856ea44c90fb
Satyam Agrawal <satyamagrawal1101@gmail.com>
2026-06-22 14:17:45 +0530

    Implement Playable interface with Saxophone and Veena

---
+++ Week 2/OOPs.java
@@ -0,0 +1,45 @@
+//Playable
+package music;
+
+public interface Playable {
+    void play();
+}
+//Saxophone
+package music.wind;
+
+import music.Playable;
+public class Saxophone implements Playable {
+    public void play() {
+        System.out.println("Saxophone is playing...");
+    }
+}
+//Test
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Veena v = new Veena();
+        v.play();
+        Saxophone s = new Saxophone();
+        s.play();
+        Playable p;
+        p = new Veena();
+        p.play();
+        p = new Saxophone();
+        p.play();
+    }
+}
+//Veena
+package music.string;
+
+import music.Playable;
+public class Veena implements Playable {
+    public void play() {
+        System.out.println("Veena is playing...");
+    }
+}
